b63189ada81daeeddcf7223ea1b5333c30378ad3
Allan Wang <me@allanwang.ca>
2019-01-09 21:49:01 -0500

    Test first travis integration

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
+import Language.HaLex.Dfa
+import Language.HaLex.Fa2RegExp
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = putStr asdf
+
+asdf :: String
+asdf = show $ dfa2RegExp dfa
+  where
+    regex = dfa2RegExp dfa
+    dfa = Dfa ['a', 'b'] [0, 1, 2, 3] 0 [0] delta
+    delta 0 'a' = 1
+    delta 0 'b' = 2
+    delta 1 'a' = 0
+    delta 1 'b' = 3
+    delta 2 'a' = 3
+    delta 2 'b' = 0
+    delta 3 'a' = 2
+    delta 3 'b' = 1
